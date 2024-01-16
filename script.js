fd884e33f12fb00a1f1cde44973a43f1d5591273
patricklouback <patricklouback@hotmail.com>
2024-01-16 15:26:16 -0300

    Organizando html

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var button = document.getElementById("button")
 button.addEventListener("click",calcularSistema)
 function calcularSistema () {
     var consumo = parseFloat(document.getElementById("consumo").value);
+    var linkPreco = document.getElementById("linkPreco");
 
     var tamanhoSistema = consumo / 30 / 3.8;
+    var newLink = "https://www.aldo.com.br/categoria/energia-solar/gerador-de-energia-solar-fotovoltaico/on-grid/growatt?filtro=3:" + tamanhoSistema.toFixed(1) + "~30000"
 
-    document.getElementById("resultado").innerText = tamanhoSistema.toFixed(2) + "kWp";
+    console.log(tamanhoSistema.toFixed(2))
+    console.log(newLink)
+
+    document.getElementById("resultado").innerText = consumo ? tamanhoSistema.toFixed(2).replace(".", ",") + " kWp" : " - kWp";
+    linkPreco.style.display = "inline"
+    linkPreco.href = newLink
+
 }});
